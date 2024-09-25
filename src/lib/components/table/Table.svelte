6e5b1911a762a65d2e0a823697abbef280570170
molarmanful <benjinpang@gmail.com>
2024-09-25 16:06:41 -0400

    insanity

--- src/lib/components/table/Table.svelte
+++ src/lib/components/table/Table.svelte
@@ -2,6 +2,17 @@
   import umap from '$lib/Unicode'
 </script>
 
-{#each umap.entries() as [code, meta]}
-  <pre>{code.toString(16).padStart(4, '0')}: {meta.name}</pre>
-{/each}
+<div class='mx-auto container'>
+  <div
+    style:grid-template-columns='repeat(auto-fill, minmax(8ch, 1fr))'
+    class='grid gap-1px b-(1 black) bg-black [&>*]:(bg-white p-1 text-center)'
+  >
+    {#each umap.entries() as [code]}
+      <div>
+        {code.toString(16).padStart(4, '0')}
+        <br>
+        <code class='whitespace-pre-wrap text-2xl'>{String.fromCodePoint(code)}</code>
+      </div>
+    {/each}
+  </div>
+</div>
